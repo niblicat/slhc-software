e580e383263356348b32797fd2f6eb5cdbdc6022
Jared Christopher <jarchri@siue.edu>
2024-09-29 22:23:11 -0500

    Added Google AUTH: Error 400: redirect_uri_mismatch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,113 @@
 <script lang="ts">
-    export let data;
-
-    $: pets = data.pets.map(row => ({
-        name: row.name,
-        owner: row.owner
-    })) as Array<{ name: string; owner: string }>;
-
-    let checker = false;
-    function DoSomething() {
-        checker = !checker;
-    }
-
-
+    import { page } from "$app/stores"
+    import { SignIn } from "@auth/sveltekit/components"
+    import { signIn } from "@auth/sveltekit/client"
+  
+    let password = ""
 </script>
-<header class="flex justify-between bg-gray-200 p-2 items-center text-gray-600 border-b-2">
-	<nav class="flex">
-		<a class="block" href="/">Tester Page</a>
-		<a class="block" href="/home">Home</a>
-		<a class="block" href="/login">Login</a>
-	</nav> 
-	
-</header>
 
-<h1>welcome to the jungle</h1>
+<h1>SvelteKit Auth Example</h1>
+<div class="container">
+  <p>
+    This is an example site to demonstrate how to use
+    <a href="https://kit.svelte.dev/">SvelteKit</a>
+    with <a href="https://sveltekit.authjs.dev">SvelteKit Auth</a> for authentication.
+  </p>
 
-<button
-on:click={() => {
-    DoSomething();
-}}>
-do something
-</button>
+  <div class="session-code">
+    <div class="session-code-header">
+      <h3>Session</h3>
+    </div>
+    <div class="session-code-body">
+      <pre>
+{JSON.stringify($page.data.session, null, 2)}
+      </pre>
+    </div>
+  </div>
 
-<input type="checkbox" checked={checker}>
-
-<h2>database test</h2>
-<div>
-    <p>my pets (if you do not see any pets, something is wrong)</p>
-    <ul>
-        {#each pets as { name, owner } (name)}
-            <li><div>{name} - {owner}</div></li>
-        {/each}
-    </ul>
+  <div class="login-cards">
+    <div class="card">
+      <!-- <div class="card-header">
+        <h3>Server</h3>
+      </div> -->
+      <div class="card-body">
+        <!-- <p>
+          These actions are all using the components exported from
+          <code>@auth/sveltekit/components</code> to run via form actions.
+        </p> -->
+        <div class="actions">
+          <SignIn provider="google">
+            <span slot="submitButton">
+              <img
+                src="https://authjs.dev/img/providers/google.svg"
+                alt="Google Logo"
+                width="20"
+                height="20"
+              />
+              Google
+            </span>
+          </SignIn>
+          <div class="or-split">or</div>
+          <!-- <SignIn provider="credentials">
+            <span slot="submitButton">Sign In with Credentials</span>
+            <div slot="credentials" style="width: 100%;">
+              <div class="wrapper-form" style="width: 100%;">
+                <div class="input-wrapper">
+                  <label for="server-password">Password</label>
+                  <input
+                    type="password"
+                    id="server-password"
+                    name="password"
+                    placeholder="password"
+                    required
+                  />
+                </div>
+              </div>
+            </div>
+          </SignIn> -->
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-header">
+        <h3>Client</h3>
+      </div>
+      <div class="card-body">
+        <p>
+          These actions are all using the methods exported from
+          <code>@auth/sveltekit/client</code>
+        </p>
+        <div class="actions">
+          <div class="wrapper-form social-btn">
+            <button on:click={() => signIn("google")}>
+              <img
+                src="https://authjs.dev/img/providers/google.svg"
+                alt="Google Logo"
+                width="20"
+                height="20"
+              />
+              Google
+            </button>
+          </div>
+          <div class="or-split">or</div>
+          <div class="wrapper-form">
+            <div class="input-wrapper">
+              <label for="client-password">Password</label>
+              <input
+                bind:value={password}
+                type="password"
+                placeholder="password"
+                id="client-password"
+                name="password"
+                required
+              />
+            </div>
+            <button on:click={() => signIn("credentials", { password })}>
+              Sign In with Credentials
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
