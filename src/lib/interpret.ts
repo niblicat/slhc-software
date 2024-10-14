97a5fce5c9467f7cdd2354982386204031a6d435
niblicat <121634408+niblicat@users.noreply.github.com>
2024-10-14 16:01:12 -0500

    iffy hearing report generation

--- src/lib/interpret.ts
+++ src/lib/interpret.ts
@@ -1,14 +1,41 @@
 // interpret.ts
 // Used to intepret user data and detect if there has been STS
 
+import { left } from "@popperjs/core";
+import { error } from "@sveltejs/kit";
+
+function findAverage(...args: number[]) {
+    let sum = 0;
+    for (var i = 0; i < args.length; ++i)
+        sum += args[i]
+
+    let average = sum / args.length
+    return average;
+}
+
 // used to identify hearing anomolies for each ear
 enum AnomolyStatus {
     none,
+    base,
     same,
     better,
     worse,
     STS,
-    other
+    warning
+}
+
+class EarAnomolyStatuses {
+    leftStatus: AnomolyStatus;
+    rightStatus: AnomolyStatus;
+    baseYear: number;
+    comparedYear: number;
+
+    constructor(leftStatus: AnomolyStatus, rightStatus: AnomolyStatus, baseYear: number, comparedYear: number) {
+        this.leftStatus = leftStatus;
+        this.rightStatus = rightStatus;
+        this.baseYear = baseYear;
+        this.comparedYear = comparedYear;
+    }
 }
 
 class HearingDataOneEar {
@@ -55,10 +82,47 @@ class UserHearingScreeningHistory {
     }
 
     /**
+     * GetStatusForEar
+     * Returns the anomoly status for an ear given data for two years
+     */
+    private GetStatusForEar(beforeEarData: HearingDataOneEar, afterEarData: HearingDataOneEar) {
+        // Get the status for one ear
+        let unweighedAverageChange = this.GetAverageDecibalChangeForOneEar(beforeEarData, afterEarData);
+        let weighedAverageChange = this.GetAverageDecibalChangeForOneEarForMainLevels(beforeEarData, afterEarData);
+
+        // a larger value is worse
+        if (weighedAverageChange >= 10) return AnomolyStatus.STS;
+        if (unweighedAverageChange >= 10) return AnomolyStatus.warning;
+        else if (unweighedAverageChange >= 5) return AnomolyStatus.worse;
+        else if (unweighedAverageChange == 0) return AnomolyStatus.same;
+        else return AnomolyStatus.better;
+    }
+
+    /**
      * GenerateHearingReport
      */
     public GenerateHearingReport() {
-        
+        let arrayLength = this.screenings.length;
+        if (arrayLength == 0) throw error; // TODO: throw specific error
+
+        let reportArray: EarAnomolyStatuses[] = [];
+        let bestScreening = this.screenings[0];
+
+        // push base status for the first hearing screening
+        reportArray.push(new EarAnomolyStatuses(AnomolyStatus.base, AnomolyStatus.base, -1, this.screenings[0].year));
+
+        for (var i = 1; i < arrayLength; i++) {
+            let beforeYear: HearingScreening = this.screenings[i - 1];
+            let afterYear: HearingScreening = this.screenings[i];
+
+            let leftAnomolyStatus = this.GetStatusForEar(beforeYear.leftEar, afterYear.leftEar);
+            let rightAnomolyStatus = this.GetStatusForEar(beforeYear.rightEar, afterYear.rightEar);
+
+            let currentAnomolyStatuses = new EarAnomolyStatuses(leftAnomolyStatus, rightAnomolyStatus, beforeYear.year, afterYear.year);
+            reportArray.push(currentAnomolyStatuses);
+
+        }
+        // TODO: generate a list of anomoly statuses for each year
     }
 
     /**
@@ -66,16 +130,34 @@ class UserHearingScreeningHistory {
      * Used to find the average decibal change in a specific ear to detect STS
      * STS is detected if there was an average of 10+ dB change
      */
-    private GetAverageDecibalChangeForOneEar(hdata1: HearingDataOneEar, hdata2: HearingDataOneEar): AnomolyStatus {
+    private GetAverageDecibalChangeForOneEarForMainLevels(hdata1: HearingDataOneEar, hdata2: HearingDataOneEar): number {
         // ONLY CHECK 2k, 3k, 4k
         // TODO: use age table?
         let diff2000 = hdata2.hz2000 - hdata1.hz2000;
         let diff3000 = hdata2.hz3000 - hdata1.hz3000;
         let diff4000 = hdata2.hz4000 - hdata1.hz4000;
 
-        let average = (diff2000 + diff3000 + diff4000) / 3;
+        let average = findAverage(diff2000, diff3000, diff4000);
 
-        // TODO: CORRECT THE STATUS
-        return AnomolyStatus.none;
+        return average;
+    }
+    /**
+     * GetAverageDecibalChangeSTS
+     * Used to find the average decibal change in a specific ear to detect overall hearing changes
+     */
+    private GetAverageDecibalChangeForOneEar(hdata1: HearingDataOneEar, hdata2: HearingDataOneEar): number {
+        // ONLY CHECK 2k, 3k, 4k
+        // TODO: use age table?
+        let diff500 = hdata2.hz500 - hdata1.hz1000;
+        let diff1000 = hdata2.hz1000 - hdata1.hz1000;
+        let diff2000 = hdata2.hz2000 - hdata1.hz2000;
+        let diff3000 = hdata2.hz3000 - hdata1.hz3000;
+        let diff4000 = hdata2.hz4000 - hdata1.hz4000;
+        let diff6000 = hdata2.hz6000 - hdata1.hz6000;
+        let diff8000 = hdata2.hz8000 - hdata1.hz8000;
+
+        let average = findAverage(diff500, diff1000, diff2000, diff3000, diff4000, diff6000, diff8000);
+
+        return average;
     }
 }
