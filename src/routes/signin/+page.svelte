4ddd3ef5642563f20b1041ce4272eb933e14da8f
Jared Christopher <jarchri@siue.edu>
2024-10-14 15:23:58 -0500

    Fixed Sign-in redirect

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -18,7 +18,7 @@
 <div class="signin">
     <div class="card">
         <div class="provider">
-            <form action="http://localhost:5173/auth/signin/google" method="POST">
+            <form action="/auth/signin/google" method="POST">
                 <input type="hidden" name="csrfToken" value={form?.csrfToken}>
                 <input type="hidden" name="callbackUrl" value="/dashboard"> <!-- Redirect to home page after sign-in -->
                 <button type="submit" class="button" style="--provider-bg: #fff; --provider-bg-hover: color-mix(in srgb, #1a73e8 30%, #fff); --provider-dark-bg: #161b22; --provider-dark-bg-hover: color-mix(in srgb, #1a73e8 30%, #000);" tabindex="0">
