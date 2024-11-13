665df15a63753b53ad86004a03c0683303a2cb61
jaymegreer <126107259+jaymegreer@users.noreply.github.com>
2024-11-13 15:43:58 -0600

    tailwind style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,32 @@
-import flowbitePlugin from 'flowbite/plugin'
+/** @type {import('tailwindcss').Config} */
+const flowbitePlugin = require('flowbite/plugin');
 
-import type { Config } from 'tailwindcss';
-
-export default {
-    content: ['./src//*.{html,js,svelte,ts}', './node_modules/flowbite-svelte//*.{html,js,svelte,ts}'],
+module.exports = {
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
+  ],
   darkMode: 'selector',
-    theme: {
-        extend: {
-          colors: {
-            // flowbite-svelte
-            funky: '#FF5500',
-            'dark_red': '#a60528',
-            'dark_blue': '#0a5180',
-            'light_red': '#ff6385',
-            'light_blue': '#36a3eb',
-            'light_bluegreen': '#4bc0c0',
-            'dark_bluegreen': '#0a5c5c',
-            //'button': ''
-            // 'baseline': ''
-            // 'change': ''
-          }
-        }
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
     },
-
-    plugins: [flowbitePlugin]
-} as Config;
+    colors: {
+      'funky': '#FF5500',
+      'dark-red': '#a60528',
+      'dark-blue': '#0a5180',
+      'light-red': '#ff6385',
+      'light-blue': '#36a3eb',
+      'light-bluegreen': '#4bc0c0',
+      'dark-bluegreen': '#0a5c5c',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    }
+  },
+  plugins: [flowbitePlugin],
+};
