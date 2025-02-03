aa9c89ecbe03822657d1419cedc7dfdeb8830dc5
Jared Christopher <jarchri@siue.edu>
2025-02-03 12:19:26 -0600

    removed login

--- src/routes/login/+layout.server.ts
+++
@@ -1,8 +0,0 @@
-import type { LayoutServerLoad } from "./$types"
-
-// Loads serverside any data we need
-export const load: LayoutServerLoad = async (event) => {
-  return {
-    session: await event.locals.auth()
-  }
-}
