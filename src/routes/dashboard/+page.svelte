7aeaad96f77d735f496759ea532d4bc526e44b50
niblicat <121634408+niblicat@users.noreply.github.com>
2025-02-24 09:41:26 -0600

    Testing button colors on vercel

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -15,6 +15,7 @@
     import AdminPage from '$lib/AdminPage.svelte';
 	import type { UserSimple } from '$lib/MyTypes.js';
 	import Information from '$lib/Information.svelte';
+	import { Button } from 'flowbite-svelte';
     let { data } = $props();
 
     let activeURL = $derived(page.url.pathname);
@@ -57,3 +58,22 @@
         <Information />
     {/if}
 </div>
+
+<Button color="primary" class="!bg-primary-700 fixed top-2.5 left-5 z-50 w-8 h-10" on:click={() => alert("Remove this in production")}>
+    Test Color 1
+</Button>
+<Button color="primary" class="bg-primary-700 fixed top-2.5 left-5 z-50 w-8 h-10" on:click={() => alert("Remove this in production")}>
+    Test Color 2
+</Button>
+<Button color="primary" class="!bg-primary-200 fixed top-2.5 left-5 z-50 w-8 h-10" on:click={() => alert("Remove this in production")}>
+    Test Color 3
+</Button>
+<Button color="primary" class="bg-primary-200 fixed top-2.5 left-5 z-50 w-8 h-10" on:click={() => alert("Remove this in production")}>
+    Test Color 4
+</Button>
+<Button color="red" class="fixed top-2.5 left-5 z-50 w-8 h-10" on:click={() => alert("Remove this in production")}>
+    Test Color 5
+</Button>
+<Button color="primary" on:click={() => alert("Remove this in production")}>
+    Test Color 6
+</Button>
