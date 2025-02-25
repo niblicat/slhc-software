756c8af621eedd8aeb6e32500ffaaa56e6a13c5e
jaymegreer <126107259+jaymegreer@users.noreply.github.com>
2025-02-24 23:07:58 -0600

    more info for mailing info page

--- src/lib/Information.svelte
+++ src/lib/Information.svelte
@@ -54,15 +54,16 @@
     <AccordionItem>
         <span slot="header">Learn about the Mailings Page Page</span>
         <Li>All employees are listed by their database-assigned Employee ID.</Li>
+        <Li>Before starting your mail merge, ensure that your default email reader is set to <em>Outlook<em>.</Li>
         <Li>Below the employee information, you will find two buttons: "Create CSV" and "Download Template."</Li>
         <Li>First, click "Export to CSV" to download a file named <em>employees.csv</em>. You do not need to open this file, but ensure you remember its download location.</Li>
-        <Li>Next, click "Download Template" to download a document titled <em>SLHC Email Template</em>. Open this file in Microsoft Word.</Li>
+        <Li>Next, click "Download Template" to download a document titled <em>SLHC Email Template</em>. Open this file in Microsoft Word. <b>DO NOT MODIFY THE TEMPLATE!<b></Li>
         <Li>In the Word document, navigate to the "Mailings" tab and select the "Start Mail Merge" button.</Li>
-        <Li>From the "Start Mail Merge" dropdown menu, ensure "Email Messages" is selected, then close the dropdown.</Li>
+        <Li>From the "Start Mail Merge" dropdown menu, ensure "Letter" is selected, then close the dropdown.</Li>
         <Li>Click "Select Recipients" and choose "Use an Existing List."</Li>
-        <Li>When prompted, locate and select the previously downloaded <em>employees.csv</em> file.</Li>
-        <Li>You may be prompted to edit or filter the recipient list, but this step is optional.</Li>
-        <Li>Also optionally, you can click "Preview Results" to review how the employee data appears in the template.</Li>
-        <Li>Finally, click "Finish & Merge," then select "Merge to E-Mail" to complete the process.</Li>
+        <Li>When prompted, locate and select the previously downloaded <em>employees.csv</em> file and click open.</Li>
+        <Li>You may be prompted to edit or filter the recipient list, but this step is optional. For our purposes, we do not have to use these features, so you can skip this step.</Li>
+        <Li>Also optionally, you can click "Preview Results" to review how the employee data appears in the template. Using this feature will auto fill the employees data into the fields specified on the template. After using this feature, all data fields should be filled with the data stored in <em>employees.csv</em> and everything should look formatted properly.</Li>
+        <Li>Finally, click "Finish & Merge," then select "Merge to E-Mail". Select the <em>To</em> dropdown window and select <em>Email</em>. The <em>Subject</em> field will give your email a subject and it will be the same for all employees (ex. Subject: Hearing Test Results). In the <em>Send As</em> field, select <em>HTML Message</em> in the dropdown window.</Li> 
     </AccordionItem>
 </Accordion>
