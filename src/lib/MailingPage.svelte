8bb92df8e8a605d3013938678a1ef7ab1ea136d2
niblicat <121634408+niblicat@users.noreply.github.com>
2025-02-26 13:00:47 -0600

    added loading icon when waiting for csv download

--- src/lib/MailingPage.svelte
+++ src/lib/MailingPage.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { ButtonGroup, Button, Footer } from 'flowbite-svelte';
+    import { ButtonGroup, Button, Footer, Spinner } from 'flowbite-svelte';
     import type { Employee } from './MyTypes';
     import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell, Checkbox, Modal, Tooltip } from 'flowbite-svelte';
     import { AnomalyStatus } from "./interpret";
@@ -13,6 +13,8 @@
     // No need for employeesMap anymore; we directly work with the employees array
     let success = $state(true);
     let errorMessage = $state("");
+    let loadingTemplate = $state(false);
+    let loadingCSV = $state(false);
 
     let STSstatusRight = $state("No data selected");
     let STSstatusLeft = $state("No data selected");
@@ -232,7 +234,11 @@
     // Handle export functionality
     async function handleExport() {
         const employeeList = createEmployeeList(); // Get the list of all employees
+        loadingCSV = true;
+        document.body.style.cursor='wait';
         const csvContent = await createCSV(employeeList); // Generate CSV content
+        document.body.style.cursor='auto';
+        loadingCSV = false;
 
         if (employeeList.length > 0) {
             downloadCSV(csvContent); // Call the function to download the CSV
@@ -242,7 +248,7 @@
     }
 
     // Function to trigger file download
-    function downloadCSV(csvContent: string) {
+    async function downloadCSV(csvContent: string) {
         const blob = new Blob([csvContent], { type: 'text/csv' });
         const url = URL.createObjectURL(blob);
         const a = document.createElement('a');
@@ -297,8 +303,8 @@
             <Button color="primary" class="w-[50%] cursor-pointer" href="/SLHC Email Template.docx">
                 Download Template
             </Button>
-            <Button color="primary" class="w-[50%] cursor-pointer" on:click={handleExport}>
-                Export to CSV
+            <Button disabled={loadingCSV} color="primary" class="w-[50%] {loadingCSV ? "" : "cursor-pointer"}" on:click={handleExport}>
+                {#if loadingCSV}<Spinner class="me-3" size="4" color="white" />{/if}Export to CSV
             </Button>
         </ButtonGroup>
     </div>
