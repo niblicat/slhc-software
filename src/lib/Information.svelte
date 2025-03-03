aa762096e3af515277c43a86c8fff7568c24b6b9
niblicat <121634408+niblicat@users.noreply.github.com>
2025-03-03 11:20:34 -0600

    only show information

--- src/lib/Information.svelte
+++ src/lib/Information.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
-    import { Accordion, AccordionItem, Li } from "flowbite-svelte";
+    import { Accordion, AccordionItem, Li, List } from "flowbite-svelte";
     import { PageCategory, type InformationPerCategory } from "./MyTypes";
     import { getInformationPerCategory } from "./infopercategory.svelte";
-
-    let informationPerCategory: InformationPerCategory[] | null = getInformationPerCategory();
 
     interface Props {
         page?: PageCategory
@@ -11,29 +9,56 @@
     const defaultCategory = PageCategory.Home;
 
     let {page = defaultCategory}: Props = $props();
+
+    let informationPerCategory: InformationPerCategory[] | null = getInformationPerCategory();
+    let singleInformation: InformationPerCategory | undefined = $derived.by(() => {
+        return informationPerCategory?.find((item) => item.category === page);
+    })
 </script>
 
-{#if informationPerCategory}
-    <Accordion>
-        {#each informationPerCategory as main}
-            <AccordionItem class="cursor-pointer">
-                <span slot="header">{main.header}</span>
-                {#each main.points as point}
-                    <Li>{@html point}</Li>
-                {/each}
-                {#if main.sub}
-                    <Accordion class="bg-gray-100">
-                        {#each main.sub as sub}
-                            <AccordionItem class="cursor-pointer">
-                            <span slot="header">{sub.header}</span>
-                            {#each sub.points as point}
-                                <Li>{@html point}</Li>
+{#if page == PageCategory.Home}
+    {#if informationPerCategory}
+        <Accordion>
+            {#each informationPerCategory as main}
+                <AccordionItem class="cursor-pointer">
+                    <span slot="header">{main.header}</span>
+                    {#each main.points as point}
+                        <Li>{@html point}</Li>
+                    {/each}
+                    {#if main.sub}
+                        <Accordion class="bg-gray-100">
+                            {#each main.sub as sub}
+                                <AccordionItem class="cursor-pointer">
+                                <span slot="header">{sub.header}</span>
+                                {#each sub.points as point}
+                                    <Li>{@html point}</Li>
+                                {/each}
+                            </AccordionItem>
                             {/each}
-                        </AccordionItem>
-                        {/each}
-                    </Accordion>
-                {/if}
-            </AccordionItem>
-        {/each}
-    </Accordion>
+                        </Accordion>
+                    {/if}
+                </AccordionItem>
+            {/each}
+        </Accordion>
+    {/if}
+{:else}
+    {#if singleInformation}
+        <List position="inside">
+            {#each singleInformation.points as point}
+            <Li>{@html point}</Li>
+            {/each}
+        </List>
+        {#if singleInformation.sub}
+            <Accordion class="bg-gray-100">
+                {#each singleInformation.sub as sub}
+                    <AccordionItem class="cursor-pointer">
+                    <span slot="header">{sub.header}</span>
+                    {#each sub.points as point}
+                        <Li>{@html point}</Li>
+                    {/each}
+                </AccordionItem>
+                {/each}
+            </Accordion>
+        {/if}
+    {/if}
 {/if}
