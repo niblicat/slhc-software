92ab9714587ebf9faf2c7e3154876aaf06fb5343
niblicat <121634408+niblicat@users.noreply.github.com>
2025-03-16 15:24:44 -0500

    adjusted configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
 
     return {
         plugins: [sveltekit(), tailwindcss()],
